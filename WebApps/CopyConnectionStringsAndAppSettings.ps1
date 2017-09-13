949acea8384633741377e661b7454248bd8d3965
Jason Haley <jason@jasonhaley.com>
2017-09-13 15:45:35 -0400

    Initial add

---
+++ WebApps/CopyConnectionStringsAndAppSettings.ps1
@@ -0,0 +1,44 @@
+﻿$subscriptionId = "<subscriptionId>"
+$resourceGroupSource = "<source resource group>"
+$resourceGroupTarget = "<target resource group>"
+
+$webAppsource = "<source web app name>"
+$webAppTarget = "<target web app name>"
+
+$slotSource = "<source slot>"
+$slotTarget = "<target slot>"
+
+Login-AzureRmAccount
+
+Set-AzureRmContext -SubscriptionId $subscriptionId
+
+# Load Existing Web App settings for source and target
+$webAppSource = Get-AzureRmWebAppSlot -ResourceGroupName $resourceGroupSource -Name $webAppsource -Slot $slotSource
+
+# Get reference to the source Connection Strings
+$connectionStringsSource = $webAppSource.SiteConfig.ConnectionStrings
+
+# Create Hash variable for Connection Strings
+$connectionStringsTarget = @{}
+
+# Copy over all Existing Connection Strings to the Hash
+ForEach($connStringSource in $connectionStringsSource) {
+    $connectionStringsTarget[$connStringSource.Name] = @{ Type = $connStringSource.Type.ToString(); Value = $connStringSource.ConnectionString }
+}
+
+# Save Connection Strings to Target
+Set-AzureRmWebAppSlot -ResourceGroupName $resourceGroupTarget -Name $webAppTarget -Slot $slotTarget -ConnectionStrings $connectionStringsTarget
+
+# Get reference to the source app settings
+$appSettingsSource = $webAppSource.SiteConfig.AppSettings
+
+# Create Hash variable for App Settings
+$appSettingsTarget = @{}
+
+# Copy over all Existing App Settings to the Hash
+ForEach ($appSettingSource in $appSettingsSource) {
+    $appSettingsTarget[$appSettingSource.Name] = $appSettingSource.Value
+}
+
+# Save Connection Strings to Target
+Set-AzureRmWebAppSlot -ResourceGroupName $resourceGroupTarget -Name $webAppTarget -Slot $slotTarget -AppSettings $appSettingsTarget
---
+++ WebApps/ExportConnectionStringsAndAppSettings.ps1
@@ -0,0 +1,23 @@
+﻿$subscriptionId = "<subscriptionId>"
+$resourceGroupSource = "<source resource group>"
+$webAppsource = "<source web app name>"
+$slotSource = "<source slot>"
+
+$appSettingsFileName = "appSettings.csv"
+$connectionStringsFileName = "connectionStrings.csv"
+
+Login-AzureRmAccount
+
+Set-AzureRmContext -SubscriptionId $subscriptionId
+
+# Load Existing Web App settings for source and target
+$webAppSource = Get-AzureRmWebAppSlot -ResourceGroupName $resourceGroupSource -Name $webAppsource -Slot $slotSource
+
+# Create csv files if file names are set
+If ($appSettingsFileName -ne "") {
+    $webAppsource.SiteConfig.AppSettings | Select-Object -Property Name, Value | Export-Csv -Path $appSettingsFileName -NoTypeInformation
+}
+
+If ($connectionStringsFileName -ne "") {
+    $webAppsource.SiteConfig.ConnectionStrings | Select-Object -Property Name, Type, ConnectionString | Export-Csv -Path $connectionStringsFileName -NoTypeInformation
+}
